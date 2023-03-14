e5bd8ddce260380f682d3cc69f2b16663c811f8b
Hazel <hazel@departure.dev>
2023-03-14 15:08:26 -0400

    use chacha instead

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,11 @@ use std::sync::{Arc, Mutex};
 use arc_swap::ArcSwap;
 use byteorder::{BigEndian, ReadBytesExt};
 use hex::encode;
-use nanorand::{Rng, WyRand};
+use rand_chacha::ChaCha20Rng;
+use rand_chacha::rand_core::{RngCore, SeedableRng};
 
 thread_local! {
-    static RNG : ArcSwap<Mutex<WyRand>> = ArcSwap::new(Arc::new(Mutex::new(WyRand::new_seed(11233429492))));
+    static RNG : ArcSwap<Mutex<ChaCha20Rng>> = ArcSwap::new(Arc::new(Mutex::new(ChaCha20Rng::seed_from_u64(11233429492))));
 }
 
 pub fn set_seed(seed: [u8; 32]) {
@@ -18,7 +19,7 @@ pub fn set_seed(seed: [u8; 32]) {
 
 pub fn set_seed_u64(seed: u64) {
     RNG.with(|v| {
-        v.swap(Arc::new(Mutex::new(WyRand::new_seed(seed))));
+        v.swap(Arc::new(Mutex::new(ChaCha20Rng::seed_from_u64(seed))));
     })
 }
 
@@ -27,24 +28,25 @@ pub fn uuid16() -> String {
     let bytes = RNG.with(|rng| {
         let rng = rng.load();
         let mut rng = rng.lock().unwrap();
+
+        let mut part_one: [u8; 4] = Default::default();
+        let mut part_two: [u8; 2] = Default::default();
+        let mut part_three: [u8; 2] = Default::default();
+        let mut part_four: [u8; 2] = Default::default();
+        let mut part_five: [u8; 6] = Default::default();
+
+        rng.fill_bytes(&mut part_one);
+
+        rng.fill_bytes(&mut part_two);
+        rng.fill_bytes(&mut part_three);
+        rng.fill_bytes(&mut part_four);
+        rng.fill_bytes(&mut part_five);
         return (
-            encode([
-                rng.generate::<u8>(),
-                rng.generate::<u8>(),
-                rng.generate::<u8>(),
-                rng.generate::<u8>(),
-            ]), //4
-            encode([rng.generate::<u8>(), rng.generate::<u8>()]), //2
-            encode([rng.generate::<u8>(), rng.generate::<u8>()]), //2
-            encode([rng.generate::<u8>(), rng.generate::<u8>()]), //2
-            encode([
-                rng.generate::<u8>(),
-                rng.generate::<u8>(),
-                rng.generate::<u8>(),
-                rng.generate::<u8>(),
-                rng.generate::<u8>(),
-                rng.generate::<u8>(),
-            ]), //6
+            encode(part_one), //4
+            encode(part_two), //2
+            encode(part_three), //2
+            encode(part_four), //2
+            encode(part_five), //6
         );
     });
 
@@ -67,7 +69,7 @@ fn test_uuid16() {
 fn test_set_seed_u64() {
     set_seed_u64(123);
     let id: String = uuid16();
-    assert_eq!("1859d40f-2c26-22ce-da23-33be037553d0", id)
+    assert_eq!("d6daf4ca-bee9-f9f9-4b53-f7f1d8e813e4", id)
 }
 
 // Check against known value
@@ -79,5 +81,5 @@ fn test_set_seed_bytes() {
     ]);
 
     let id: String = uuid16();
-    assert_eq!("8e6da460-de1f-29a2-570a-7e8e3ac51a99", id)
+    assert_eq!("b2f7f581-d6de-a822-7e82-c00f8401696a", id)
 }
